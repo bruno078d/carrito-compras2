d6b3cee34119959b956aeabe9235ff4779521b93
bruno078d <brunomettifogo@institutocedec.com>
2025-04-28 16:51:05 -0300

    03- gestion de datos

--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product,products} from '../products';
+import { CarritoService } from '../carrito.service';
 @Component({
   selector: 'app-detalle-producto',
   templateUrl: './detalle-producto.component.html',
   styleUrl: './detalle-producto.component.css'
 })
 export class DetalleProductoComponent implements OnInit {
-producto: Product | undefined;
+  producto: Product | undefined;
 
-constructor(private ruta: ActivatedRoute) {}
+  constructor(
+    private ruta: ActivatedRoute,
+    private carritoService: CarritoService)
+  {}
 
-ngOnInit(): void {
-  const paramRuta = this.ruta.snapshot.paramMap;
-  const idProducto = Number(paramRuta.get('id'));
-  
-  this.producto = products.find(producto => producto.id === idProducto)
+  ngOnInit(): void {
+    const paramRuta = this.ruta.snapshot.paramMap;
+    const idProducto = Number(paramRuta.get('id'));
+    
+    this.producto = products.find(producto => producto.id === idProducto)
+  }
+
+  agregarAlCarrito(producto : Product){
+    this.carritoService.agregarAlCarrito(producto)
+    window.alert('El producto se ha agregado al carrito')
+  }
 }
-}
-
 
 
 
